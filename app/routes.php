c469a50c3db1a22f659ec0b0a419e35809632de7
USAMI Kenta <tadsan@zonu.me>
2019-10-07 21:08:52 +0900

    404 NotFoundも関数として同じように扱うようにした

--- app/routes.php
+++ app/routes.php
@@ -3,11 +3,17 @@
 $routes = [];
 
 $routes['/'] = function () {
+    ob_start();
     include __DIR__ . '/../app/view/index.phtml';
+
+    return [200, ['Content-Type' => 'text/html'], ob_get_clean()];
 };
 
 $routes['/phpinfo.php'] = function () {
+    ob_start();
     phpinfo();
+
+    return [200, ['Content-Type' => 'text/html'], ob_get_clean()];
 };
 
 return $routes;
--- public/index.php
+++ public/index.php
@@ -6,10 +6,17 @@ $routes = require __DIR__ . '/../app/routes.php';
 
 $request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-if (isset($routes[$request_uri])) {
-    $f = $routes[$request_uri];
-    $f();
-} else {
-    http_response_code(404);
-    echo "<h1>404 Not Found</h1>";
+// after
+$not_found = function () {
+    return [404, ['Content-Type'=>'text/html'], "<h1>404 Not Found</h1>"];
+};
+
+$f = isset($routes[$request_uri]) ? $routes[$request_uri] : $not_found;
+$f = $routes[$request_uri] ?? $not_found;
+
+[$status, $headers, $body] = $f();
+http_response_code($status);
+foreach ($headers as $name => $h) {
+    header("{$name}: {$h}");
 }
+echo $body;
