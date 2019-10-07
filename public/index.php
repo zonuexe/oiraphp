b827a09ce2ce32bdaeac161c191566dcb432c434
USAMI Kenta <tadsan@zonu.me>
2019-10-07 20:22:59 +0900

    クエリパラメータがついてると404になるバグを直した

--- public/index.php
+++ public/index.php
@@ -2,23 +2,12 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-$routes = [];
+$routes = require __DIR__ . '/../app/routes.php';
 
-$routes['/'] = function () {
-    echo "<!DOCTYPE html>\n";
-    echo "<title>test</title>\n";
-    echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')). "です</p>\n";
-    echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
-    echo "<hr>\n";
-    echo "<p><a href='https://github.com/zonuexe/oiraphp'>This code is licensed under AGPL.</a></p>";
-};
+$request_uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-$routes['/phpinfo.php'] = function () {
-    phpinfo();
-};
-
-if (isset($routes[$_SERVER['REQUEST_URI']])) {
-    $f = $routes[$_SERVER['REQUEST_URI']];
+if (isset($routes[$request_uri])) {
+    $f = $routes[$request_uri];
     $f();
 } else {
     http_response_code(404);
