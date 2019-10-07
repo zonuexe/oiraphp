773a4354a7d575d09e798efbcceb518dcb15501a
USAMI Kenta <tadsan@zonu.me>
2019-10-07 20:13:29 +0900

    phpinfoを消してindex.phpだけで表示できるようにした

--- public/index.php
+++ public/index.php
@@ -2,9 +2,25 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
-echo "<!DOCTYPE html>\n";
-echo "<title>test</title>\n";
-echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')). "です</p>\n";
-echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
-echo "<hr>\n";
-echo "<p><a href='https://github.com/zonuexe/oiraphp'>This code is licensed under AGPL.</a></p>";
+$routes = [];
+
+$routes['/'] = function () {
+    echo "<!DOCTYPE html>\n";
+    echo "<title>test</title>\n";
+    echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')). "です</p>\n";
+    echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
+    echo "<hr>\n";
+    echo "<p><a href='https://github.com/zonuexe/oiraphp'>This code is licensed under AGPL.</a></p>";
+};
+
+$routes['/phpinfo.php'] = function () {
+    phpinfo();
+};
+
+if (isset($routes[$_SERVER['REQUEST_URI']])) {
+    $f = $routes[$_SERVER['REQUEST_URI']];
+    $f();
+} else {
+    http_response_code(404);
+    echo "<h1>404 Not Found</h1>";
+}
