4489faa400e4abb91657fadb3d2a51c940d11086
USAMI Kenta <tadsan@zonu.me>
2019-10-07 20:15:34 +0900

    src/index.php と app/routes.php を分割した

---
+++ app/routes.php
@@ -0,0 +1,18 @@
+<?php
+
+$routes = [];
+
+$routes['/'] = function () {
+    echo "<!DOCTYPE html>\n";
+    echo "<title>test</title>\n";
+    echo "<p>現在は" . h(date('Y年m月d日H時i分s秒')). "です</p>\n";
+    echo "<ul><li><a href='/phpinfo.php'><code>phpinfo()</code></a></ul>\n";
+    echo "<hr>\n";
+    echo "<p><a href='https://github.com/zonuexe/oiraphp'>This code is licensed under AGPL.</a></p>";
+};
+
+$routes['/phpinfo.php'] = function () {
+    phpinfo();
+};
+
+return $routes;
